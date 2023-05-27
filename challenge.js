d9be96b946e131b43ab495cb945f0335470ee48d
Sahal Dahir <sahaldahir@Sahals-MacBook-Pro.local>
2023-05-27 11:51:47 +0300

    this is my Assignment

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let x = 10;
+let y = 15;
+let z = 30;
+
+if (z > x && y) {
+  console.log("The largest number out of the three numbers is z");
+}
